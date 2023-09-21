5f21490f9a1308ec00bdd91b688f2966f51958b8
jamie saunders <jamiesaunders03@gmail.com>
2023-09-21 16:41:56 +0100

    Start tasks

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -1,13 +1,14 @@
 ;; code for Task 1.2(a):
-(print (1 + 2))
-;; the above expression throws an error
+(print (+ 1 2))
 
 ;; code for Task 1.2(b):
 (format t "type something: ")
 (setf input (read-line))
-(format t "you typed: ~a" input)
+(if (string= "" input) (print "nothing typed") (format t "you typed: ~a" input))
 
 ;; code for Task 1.2(c):
 (setf prg '(+ 1 n)) ; define a very simple program
 (print prg) ; print the program
+(setf n 1)
+(print (eval prg))
 ; TODO: execute the program with n = 1 and print its result
