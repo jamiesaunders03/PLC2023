f4bd9397abca9a6753918c86ea51603710a36ec3
jamie saunders <jamiesaunders03@gmail.com>
2023-09-28 10:11:47 +0100

    Task 3

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -7,8 +7,4 @@
 (if (string= "" input) (print "nothing typed") (format t "you typed: ~a" input))
 
 ;; code for Task 1.2(c):
-(setf prg '(+ 1 n)) ; define a very simple program
-(print prg) ; print the program
-(setf n 1)
-(print (eval prg))
-; TODO: execute the program with n = 1 and print its result
+(let ((prg '(+ 1 n))) (print prg)  (setf n 1) (print (eval prg))) ; define a very simple program
