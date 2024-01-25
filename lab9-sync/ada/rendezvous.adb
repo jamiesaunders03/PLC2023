12742f6c76aaf08c887bb43db703cbb5ad2ab65b
jamie saunders <jamiesaunders03@gmail.com>
2024-01-25 10:19:26 +0000

    Add serial to ada rendevous

--- lab9-sync/ada/rendezvous.adb
+++ lab9-sync/ada/rendezvous.adb
@@ -7,17 +7,20 @@ use Ada.Integer_Text_IO;
 procedure rendezvous is
 
     task A is
-        entry Exchange(N : in Integer; R : out Integer);
+        entry Exchange(N : in Integer; R : out Integer; S : out Integer);
     end A;
     
     task body A is
         Sum : Integer := 0;
+        Serial : Integer := 0;
     begin
         loop
-            accept Exchange(N : in Integer; R : out Integer) 
+            accept Exchange(N : in Integer; R : out Integer; S : out Integer) 
             do
                 Sum := Sum + N;
                 R := Sum;
+                S := Serial;
+                Serial := Serial + 1;
             end; -- the calling task in now released
             exit when Sum > 10;
         end loop;
@@ -28,13 +31,14 @@ procedure rendezvous is
     end A;
     
     I : Integer;
+    S : Integer;
 begin
-    A.Exchange(1,I); -- I = 1
+    A.Exchange(1,I,S); -- I = 1
     Put("I = "); Put(I); Put_Line("");
-    A.Exchange(1,I); -- I = 2
+    A.Exchange(1,I,S); -- I = 2
     Put("I = "); Put(I); Put_Line("");
-    A.Exchange(9,I); -- A will enter the dummy loop
+    A.Exchange(9,I,S); -- A will enter the dummy loop
     Put("I = "); Put(I); Put_Line("");
-    A.Exchange(1,I); -- rendezvous will never happen
+    A.Exchange(1,I,S); -- rendezvous will never happen
     Put("I = "); Put(I); Put_Line("");
 end rendezvous;
